aa6d16b43a4ec2290d7d8094c9abdf3db81bc2ba
TKOL <ddtriaankur@gmail.com>
2016-06-10 09:50:29 +0530

    day 2 changes

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-//NOTES
+/*
+NOTES
 // process.argv[2] = Name of the Database
 // process.argv[3] = Name of the Collection
 // process.argv[4] = Operation CRUD name
 // process.argv[5] = _id of the document to be removed in delete mode, name of the doc to be 'updated' mode
 // process.argv[6] = new document name
 
-//
+*/
 
 var mongo = require('mongodb').MongoClient
-var option = process.argv[4] ;  // read, insert
+var option = process.argv[4] ;						//read, insert
 
 /*
 	READ DOCUMENTS
+	node app myproject link read
 */
+var mongo = require('mongodb').MongoClient ;
 
-var mongo = require('mongodb').MongoClient;
-
-// node app myproject link read
 if(option === 'read') {
-	mongo.connect('mongodb://localhost:27017/'+ process.argv[2], function(err, db) {
-	        if(err) throw err;
-					// console.log('Connected correctly to server');
+	mongo.connect('mongodb://localhost:27017/'+ process.argv[2], function(err, db){
+	        if(err) throw err ;
+					// console.log('Connected correctly to server') ;
 
-	        // var collection = db.collection('link');
-	        var collection = db.collection(process.argv[3]);
+	        // var collection = db.collection('link') ;
+	        var collection = db.collection(process.argv[3]) ;
 
-	        collection.find({}, {"_id": "0", "name": "1"})
+	        collection.find({}, { "_id": "0", "name": "1" } )
 	        .toArray(function(err, docs) {
 	            if(err) throw err;
 
@@ -36,27 +36,29 @@ if(option === 'read') {
 
 }
 
+
 /*
 	INSERT DOCUMENTS
+	node app myproject link insert <hash> <name> 
 */
-
-// node app myproject link insert 
-if (option === 'insert') {
+if (option === 'insert'){
   mongo.connect('mongodb://localhost:27017/'+ process.argv[2], function(err, db) {
-      if(err) throw err;
+      if(err) throw err ;
 
-      var collection = db.collection(process.argv[3]),
-          obj = {_id: '2gGW7z', name: 'Inspected' };
+      var collection = db.collection(process.argv[3]) ,
+          obj = {_id: process.argv[5], name: process.argv[6] } ;
 
-      collection.insert(obj);
+      collection.insert(obj) ;
 
-      console.log(JSON.stringify(obj));
-      db.close();
-	});
-
+      console.log( JSON.stringify(obj) ) ;
+      db.close() ;
+	}) ;
 }
 
-// node app myproject link delete 2gGw7z
+
+/*
+	node app myproject link delete 2gGw7z
+*/
 if(option === 'delete' ){
 	mongo.connect('mongodb://localhost:27017/'+ process.argv[2], function(err, db) {
 	  	if(err) throw err;
@@ -72,7 +74,10 @@ if(option === 'delete' ){
 	});
 }
 
-// node app myproject link update oldName NewName
+
+/*
+	node app myproject link update oldName NewName
+*/
 if(option == "update"){
 	mongo.connect('mongodb://localhost:27017/' + process.argv[2], function(err, db) {
 		    if(err) throw err;
@@ -91,8 +96,9 @@ if(option == "update"){
 		    });
 		});
 }
-
-// complete all cases above
+/*
+	complete all cases above
+*/
 if(option === 'help') {
 
 	// for(var i = 0; i<3 ; i++)console.log('try harder');
@@ -100,8 +106,8 @@ if(option === 'help') {
 	console.log(' reading = node app myproject link read');
 	console.log(' inserting = node app myproject link insert') 
 	console.log(' deleting = node app myproject link delete 2gGw7z')
-	console.log(' deleting = node app myproject link update oldName NewName')
-
+	console.log(' update = node app myproject link update oldName NewName')
+// db.link.update({name : null}, { $set : {name : "kursa" } })
 }
 
 
